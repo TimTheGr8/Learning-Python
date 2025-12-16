0777fe89149e667c99217319ab869f53d063ce9c
TimTheGr8 <timothyjanssen4@gmail>
2025-12-15 21:18:07 -0600

    Adding the initial files

---
+++ HigherLower/higherLower.py
@@ -0,0 +1,14 @@
+import art
+import data
+
+name1, name2, description1, description2 = ""
+
+""" 
+select names from data file
+ensure the names are not the same
+assign the names and descriptions
+"""
+
+
+
+question = f"Who has more followers:\nA) {name1} {description1}\nB) {name2} {description2}"
