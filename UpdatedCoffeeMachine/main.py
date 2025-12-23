6fed6dbcdf89c2d8ccd03488e3fe012171120fa6
TimTheGr8 <tjanssen4@gmail.com>
2025-12-23 13:17:26 -0600

    Finished coffe machine

--- UpdatedCoffeeMachine/main.py
+++ UpdatedCoffeeMachine/main.py
@@ -6,3 +6,15 @@ machine = CoffeeMaker()
 menu = Menu()
 money = MoneyMachine()
 
+machineOn = True
+while machineOn:
+    userInput = input(f"What would you like to order? We have {menu.get_items()} \n").lower()
+    if userInput == "report":
+        machine.report()
+    elif userInput == "off":
+        machineOn = False
+    elif menu.find_drink(userInput):
+        drink = menu.find_drink(userInput)
+        print(f"Your total is: {drink.cost}")
+        if machine.is_resource_sufficient(drink) and money.make_payment(drink.cost):
+            machine.make_coffee(drink)
