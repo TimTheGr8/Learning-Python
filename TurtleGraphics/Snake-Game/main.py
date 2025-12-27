b59171b6e24b83feb5a6865404e8756852c2bc72
TimTheGr8 <timothyjanssen4@gmail>
2025-12-27 16:03:54 -0600

    Added the starting snake game.

---
+++ TurtleGraphics/Snake-Game/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+# Screen setup
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("PySnake")
+
+
+
+
+
+
+
+screen.exitonclick()
