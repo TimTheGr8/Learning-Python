dbb875a9e67d9d2b05acbdc7c96b14e04284e209
TimTheGr8 <timothyjanssen4@gmail>
2026-01-14 06:20:36 -0600

    Updated to catch errors.

--- Nato-Alphabet-Converter/main.py
+++ Nato-Alphabet-Converter/main.py
@@ -3,7 +3,19 @@ import pandas
 alpha_data = pandas.read_csv("Nato-Alphabet-Converter/nato_phonetic_alphabet.csv")
 natobet = {row.letter: row.code for (index, row) in alpha_data.iterrows()}
 
-word = input("Enter a word: ").upper()
+# word = input("Enter a word: ").upper()
 
-response = [natobet[letter] for letter in word]
-print(response)
+incorrect_input = True
+while incorrect_input:
+    word = input("Enter a word: ").upper()
+    try:
+        response = [natobet[letter] for letter in word]
+    except KeyError:
+        print("Please only enter letters in the alaphabet.")
+    else:
+        print(response)
+        incorrect_input = False
+
+
+# response = [natobet[letter] for letter in word]
+# print(response)
