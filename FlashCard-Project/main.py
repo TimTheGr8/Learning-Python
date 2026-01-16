84b658f67684f7d2d60a4498c542bfd8a9ec025a
TimTheGr8 <timothyjanssen4@gmail>
2026-01-16 13:16:14 -0600

    Finished the morse flashy project.

--- FlashCard-Project/main.py
+++ FlashCard-Project/main.py
@@ -1,39 +1,42 @@
 from tkinter import *
 import pandas as pd
 import random
-import time
+import pygame
 # ---------------------------- VARIABLES ------------------------------- #
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = {}
+audio_file = "FlashCard-Project/audio/"
 
 try:
-    data = pd.read_csv("FlashCard-Project/data/words_to_learn.csv")
+    data = pd.read_csv("FlashCard-Project/data/to_learn.csv")
 except FileNotFoundError:
-    data = pd.read_csv("FlashCard-Project/data/french_words.csv")
+    data = pd.read_csv("FlashCard-Project/data/morse_code.csv")
 
 to_learn = data.to_dict(orient="records")
-print(len(to_learn))
+pygame.mixer.init()
 # ---------------------------- NEW WORD ------------------------------- #
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
     canvas.itemconfig(canvas_card, image=card_front_img)
-    canvas.itemconfig(language_text, text="French", fill="black")
-    canvas.itemconfig(word_text, text=current_card["French"], fill="black")
+    canvas.itemconfig(title_text, text="Morse", fill="black")
+    canvas.itemconfig(letter_text, text=current_card["Morse"], fill="black")
+    letter_sound = pygame.mixer.Sound(f"{audio_file}{current_card['Letter'].lower()}.wav")
+    letter_sound.play()
     flip_timer = window.after(3000, flip_card)
 # ---------------------------- FLIP CARD ------------------------------- #
 def flip_card():
     global current_card
     canvas.itemconfig(canvas_card, image=card_back_img)
-    canvas.itemconfig(language_text, text="English", fill="white")
-    canvas.itemconfig(word_text, text=current_card["English"], fill="white")
+    canvas.itemconfig(title_text, text="Alphabet", fill="white")
+    canvas.itemconfig(letter_text, text=current_card["Letter"], fill="white")
 # ---------------------------- REMOVE WORD ------------------------------- #
 def remove_word():
     global current_card
     to_learn.remove(current_card)
     data = pd.DataFrame(to_learn)
-    data.to_csv("FlashCard-Project/data/words_to_learn.csv", index=False)
+    data.to_csv("FlashCard-Project/data/to_learn.csv", index=False)
     next_card()
 # ---------------------------- UI SETUP ------------------------------- #
 # ----- WINDOW ----- #
@@ -46,8 +49,8 @@ canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR
 card_front_img = PhotoImage(file="FlashCard-Project/images/card_front.png")
 card_back_img = PhotoImage(file="FlashCard-Project/images/card_back.png")
 canvas_card = canvas.create_image(400, 263, image=card_front_img)
-language_text = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-word_text = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+title_text = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+letter_text = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 # ----- BUTTONS ----- #
 wrong_image = PhotoImage(file="FlashCard-Project/images/wrong.png")
 wrong_button = Button(image=wrong_image, highlightthickness=0, bd=0, command=next_card)
